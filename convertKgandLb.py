91d30bc32817adb397feab3320ce85f5149350e5
mu71l473d <judoka_rene@hotmail.com>
2019-07-05 14:52:46 +0200

    new version

--- convertKgandLb.py
+++ convertKgandLb.py
@@ -3,24 +3,24 @@
 #imports the system module
 import sys
 
-print "this script converts lb and kg"
-print "the first parameter should be kg or lb"
+print("this script converts lb and kg")
+print("the first parameter should be kg or lb")
 
 if len(sys.argv) == 2:
 	if sys.argv[1] == "kg":
-		print "this conversion will convert kg to lb"
+		print("this conversion will convert kg to lb")
 		kg = float(raw_input("enter the amount in Kgs: "))
 		lb = kg * 2.20462262
-		print "Weight", kg, "KG = ", lb, "LB"
+		print("Weight", kg, "KG = ", lb, "LB")
 	
 	elif sys.argv[1] == "lb":
-		print "this conversion will convert lb to kg"
+		print("this conversion will convert lb to kg")
 		lb = float(raw_input("enter the amount in lbs: "))
 		kg = lb / 2.20462262
-		print "Weight", lb, "LB = ", kg, "KG"	
+		print("Weight", lb, "LB = ", kg, "KG")
 	
 	else:
-		print "invalid input"
+		print("invalid input")
 	
 else:
-	print "you didn't put the right parameters in"
+	print("you didn't put the right parameters in")
