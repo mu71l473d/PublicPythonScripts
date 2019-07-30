c014098b5816749cef88182fa05fd8adcf5fa317
mu71l473d <judoka_rene@hotmail.com>
2019-07-31 00:02:20 +0200

    new version

--- rotstring.py
+++ rotstring.py
@@ -1,14 +1,14 @@
 def caesar_encrypt(realText, step):
-	outText = []
-	cryptText = []
-	
-	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    outText = []
+    cryptText = []
+
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	for eachLetter in realText:
 		if eachLetter in ' ':
-			outText.append(' ')
-		elif eachLetter in uppercase:
-			index = uppercase.index(eachLetter)
+		    outText.append(' ')
+        elif eachLetter in uppercase:
+            index = uppercase.index(eachLetter)
 			crypting = (index + step) % 26
 			cryptText.append(crypting)
 			newLetter = uppercase[crypting]
@@ -22,11 +22,12 @@ def caesar_encrypt(realText, step):
 	return ''.join(outText)
 
 
+
 def caesar_decrypt(realText, step):
 	outText = []
-	decryptText = []
+    decryptText = []
 	
-	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 	for eachLetter in realText:
