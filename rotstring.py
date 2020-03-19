0d6566922aeae264bd6459d1a07074c55aff3b64
mu71l473d <judoka_rene@hotmail.com>
2020-03-19 13:34:33 +0100

    new version

--- rotstring.py
+++ rotstring.py
@@ -2,7 +2,7 @@
 
 from itertools import product
 from string import ascii_lowercase as alphabet
-=======
+
     uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	for eachLetter in realText:
@@ -22,14 +22,12 @@ from string import ascii_lowercase as alphabet
 			outText.append(newLetter)
     return ''.join(outText)
 
-
-
 def caesar_encrypt(realText, offset):
 	return realText.translate(str.maketrans(alphabet, alphabet[offset:] + alphabet[:offset]))
 
 def caesar_decrypt(cipherText, offset):
 	outText = []
-	decryptText = []	
+	decryptText = []
 	
 	for letter in alphabet:
 		if letter in ' ':
@@ -44,9 +42,7 @@ def caesar_decrypt(cipherText, offset):
 
 
 def caesar_brutusforce(cipherText):
-
     # # cutting edge bruteforce for caesar ciphers. I'm just taking a stab at this.
-
     for i in range(1, 26):
         result = caesar_decrypt(cipherText, i)
         temp = str(i) + ': ' + result + '\n'
